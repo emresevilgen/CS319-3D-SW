a504ccb86a25e89c783684de721f4020742acaa4
Munib Emre Sevilgen <sevilgenemre@gmail.com>
2019-11-07 18:54:56 +0300

    clean

--- src/RankingsPanel.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.*;
-
-public class RankingsPanel extends JPanel {
-    public RankingsPanel(PreGameFrame frame) {
-        // setCurrentFrame(frame);
-    }
-}
--- src/SeePlayersPanel.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.*;
-
-public class SeePlayersPanel extends JPanel {
-    public SeePlayersPanel(PreGameFrame frame) {
-        // setCurrentFrame(frame);
-    }
-}
