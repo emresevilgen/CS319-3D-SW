07bc5c09e59c732861fe273babb29fb69b43f61d
Munib Emre Sevilgen <sevilgenemre@gmail.com>
2019-11-07 20:19:14 +0300

    update

---
+++ CS319-3D-SW/src/Model/LobbyUser.java
@@ -0,0 +1,19 @@
+package Model;
+
+public class LobbyUser extends User{
+
+    boolean isReady;
+
+    public LobbyUser()
+    {
+        isReady = false;
+    }
+
+    public boolean isReady() {
+        return isReady;
+    }
+
+    public void setReady(boolean ready) {
+        isReady = ready;
+    }
+}
---
+++ CS319-3D-SW/src/Model/Mode.java
@@ -0,0 +1,59 @@
+package Model;
+
+
+public class Mode {
+    boolean shufflePlaces;
+    boolean bargain;
+    boolean secretSkills;
+    boolean invalidMovePenalty;
+    boolean loot;
+
+    public Mode(boolean shufflePlaces, boolean bargain, boolean secretSkills, boolean invalidMovePenalty, boolean loot)
+    {
+        this.shufflePlaces = shufflePlaces;
+        this.bargain = bargain;
+        this.secretSkills = secretSkills;
+        this.invalidMovePenalty = invalidMovePenalty;
+        this.loot = loot;
+    }
+
+    public boolean isShufflePlaces() {
+        return shufflePlaces;
+    }
+
+    public void setShufflePlaces(boolean shufflePlaces) {
+        this.shufflePlaces = shufflePlaces;
+    }
+
+    public boolean isBargain() {
+        return bargain;
+    }
+
+    public void setBargain(boolean bargain) {
+        this.bargain = bargain;
+    }
+
+    public boolean isSecretSkills() {
+        return secretSkills;
+    }
+
+    public void setSecretSkills(boolean secretSkills) {
+        this.secretSkills = secretSkills;
+    }
+
+    public boolean isInvalidMovePenalty() {
+        return invalidMovePenalty;
+    }
+
+    public void setInvalidMovePenalty(boolean invalidMovePenalty) {
+        this.invalidMovePenalty = invalidMovePenalty;
+    }
+
+    public boolean isLoot() {
+        return loot;
+    }
+
+    public void setLoot(boolean loot) {
+        this.loot = loot;
+    }
+}
