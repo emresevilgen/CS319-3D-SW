0355fcf23b240c09ba4443d73f3403bb4a27f704
Munib Emre Sevilgen <sevilgenemre@gmail.com>
2019-11-08 20:31:36 +0300

    maven dependencies added

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module uiComponents {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.google.gson;
+    requires retrofit2;
+    requires retrofit2.converter.gson;
 
     opens uiComponents to javafx.fxml;
     exports uiComponents;
--- src/main/java/uiComponents/SignInController.java
+++ src/main/java/uiComponents/SignInController.java
@@ -7,6 +7,17 @@ import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import rest.ApiClient;
+import rest.ApiInterface;
+import rest.models.UserResponse;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class SignInController {
 
@@ -37,8 +48,8 @@ public class SignInController {
 
     }
 
-    public void signIn(String username, String password){
-        /*ApiInterface apiService =
+    public void signIn(String username, String password) throws IOException {
+ /*       ApiInterface apiService =
                 ApiClient.getClient().create(ApiInterface.class);
 
 
@@ -56,7 +67,7 @@ public class SignInController {
                         System.out.println("EROR!1");
                     } else {
                         System.out.println(userResponse.toString());
-                        *//*Stage stage;
+                        Stage stage;
                         Parent root = null;
 
                         stage = (Stage) signInButton.getScene().getWindow();
@@ -68,7 +79,7 @@ public class SignInController {
 
                         Scene scene = new Scene(root);
                         stage.setScene(scene);
-                        stage.show();*//*
+                        stage.show();
                     }
 
 
@@ -79,10 +90,19 @@ public class SignInController {
             }
 
             @Override
-            public void onFailure(Call<UserResponse>call, Throwable t) {
+            public void onFailure(Call<UserResponse> call, Throwable t) {
                 // CurrencyLog error here since request failed
 
             }
         });*/
+        Stage stage;
+        stage = (Stage) signInButton.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader();
+        FileInputStream fileInputStream = new FileInputStream(new File("src"+File.separator+"main"+File.separator+"resources"+File.separator+"fxml"+File.separator+"MainMenu.fxml"));
+        Parent root = loader.load(fileInputStream);
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
